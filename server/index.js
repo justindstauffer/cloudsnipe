6e41bc84fa956a8440b8906289fb8cb13a8be068
Justin Stauffer <justindstauffer@gmail.com>
2020-09-24 13:46:30 -0400

    First Commit

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+const app = express()
+
+// Middleware
+app.use(bodyParser.json())
+app.use(cors())
+
+
+
+const user = require('./routes/user')
+
+app.use('/api/user', user)
+
+
+
+
+// Set port
+const port = process.env.PORT || 5000
+// Open server listener
+app.listen(port, () => console.log(`Server started on port ${port}`))
---
+++ server/routes/user.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const router = express.Router();
+const mongoose = require("mongoose");
+
+mongoose.connect(
+  "mongodb+srv://jstauff:TestMongo12345@cloudsnipe.stdtd.mongodb.net/users?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
+
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function () {
+  console.log("We are connected!");
+});
+const userSchema = new mongoose.Schema({
+  name: String,
+});
+const User = mongoose.model("User", userSchema);
+
+// *******************
+// Post Request
+router.post("/", (req, res) => {
+  const josh = new User({ name: "Josh" });
+  josh.save(function (err, josh) {
+    if (err) return console.error(err);
+    console.log("Adding ", josh.name);
+  });
+  res.send("Create Josh!");
+});
+
+// *******************
+// Get Request
+router.get("/", (req, res) => {
+  User.find(function (err, users) {
+    if (err) return console.log(err);
+    res.send(users);
+  });
+});
+
+// *******************
+// Module Exports
+module.exports = router;
